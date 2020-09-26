4d5105f6d8adcad15561861de563572cdee3f020
Henry Truong <HenryTruong1997@gmail.com>
2020-09-26 02:02:31 -0400

    Wrapped ThemeProvider around the application to get darkmode styles working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { useDarkMode } from "./DarkModeComponents/useDarkMode";
+import { GlobalStyles } from "./DarkModeComponents/GlobalStyles";
+import { lightTheme, darkTheme } from "./DarkModeComponents/Themes";
+import Toggle from "./DarkModeComponents/Toggler";
 import Roth from "./RothComponents/Roth";
 import { RothContextProvider } from "./RothComponents/rothContext";
 
 function App() {
-  return (
-    <RothContextProvider>
-      <Roth />
-    </RothContextProvider>
+  const { theme, themeToggler, mountedComponent } = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  const toggle = <Toggle theme={theme} themeToggler={themeToggler} />;
+
+  return mountedComponent ? (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <RothContextProvider>
+        <Roth toggle={toggle} />
+      </RothContextProvider>
+    </ThemeProvider>
+  ) : (
+    <div />
   );
 }
 
