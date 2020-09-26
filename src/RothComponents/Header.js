67eb5a34e02e42b036be1f8140bcb2015068769a
Henry Truong <HenryTruong1997@gmail.com>
2020-09-26 02:04:59 -0400

    Changed text from h3 to p. Also changed grid structure

--- src/RothComponents/Header.js
+++ src/RothComponents/Header.js
@@ -3,12 +3,14 @@ import React from "react";
 function Header() {
   const sevenPercentArticle =
     "https://www.thesimpledollar.com/investing/stocks/where-does-7-come-from-when-it-comes-to-long-term-stock-returns/";
+
   return (
     <div className="row">
-      <div className="col-sm-12">
-        <header className="header-container">
+      <div className="col-lg-1"></div>
+      <div className="header-container col-lg-10">
+        <header>
           <h1>Roth IRA Calculator</h1>
-          <h3>
+          <p>
             Investing early can make a big impact on your retirement savings. A
             Roth Individual Retirement Account (IRA) is beneficial for younger
             adults because their marginal tax rate is lower. All future earnings
@@ -16,9 +18,10 @@ function Header() {
             to 2009, the average annual return from the S&amp;P 500 was
             <a href={sevenPercentArticle}> 7%</a>. This accounts for dividends
             and inflation.
-          </h3>
+          </p>
         </header>
       </div>
+      <div className="col-lg-1"></div>
     </div>
   );
 }
